7498dbf6567decd007d5bd6af9164a9bd3e611b3
Akulov Aleksei <71037278+Smoking-Stell@users.noreply.github.com>
2023-04-13 04:47:41 +0300

    add MMD

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from hyppo.ksample import MMD
 
 
 chat_id = 461750643 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    val = ks_2samp(x, y)
-    return val.pvalue < alpha # Ваш ответ, True или False
+    stat, pvalue = MMD().test(x, y)
+    return pvalue < alpha # Ваш ответ, True или False
